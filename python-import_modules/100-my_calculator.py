56ca0774e53bee09970c017dff42a2a12d073709
vtiquet <valentin.tiquet@gmail.com>
2025-09-04 12:54:20 +0000

    Refactor calculator scripts for improved readability and maintainability

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) - 1 != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
