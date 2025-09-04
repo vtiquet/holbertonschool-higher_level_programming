6874668081f0ce7ab1a11098bc835227eee8fd0a
vtiquet <valentin.tiquet@gmail.com>
2025-09-04 12:55:50 +0000

    Refactor calculator script for improved variable naming and structure

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
-    op = sys.argv[2]
+    operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    if op == '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
+    
