d01c584083a3885b203e3c84666d9027f727eed8
vtiquet <valentin.tiquet@gmail.com>
2025-09-04 13:34:54 +0000

    Refactor remove_char_at function to use consistent parameter naming

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def remove_char_at(s, n):
-    if n < 0 or n >= len(s):
-        return s
+def remove_char_at(str, n):
+
+    if n < 0 or n >= len(str):
+        return (str)
 
     new_str = ""
-    for i in range(len(s)):
+    for i in range(len(str)):
         if i != n:
-            new_str += s[i]
+            new_str += str[i]
     return (new_str)
