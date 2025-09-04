3cdb0cfc4f79aa0d981fe9f0585df5f2fadffaa1
vtiquet <valentin.tiquet@gmail.com>
2025-09-04 13:32:04 +0000

    Add remove_char_at function and main script for character removal

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return s
+    
+    new_str = ""
+    for i in range(len(s)):
+        if i != n:
+            new_str += s[i]
+    return (new_str)
