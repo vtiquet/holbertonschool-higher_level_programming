5e4283a4d109ec165be55fa4f14e4fe5d3291fae
vtiquet <valentin.tiquet@gmail.com>
2025-09-08 09:25:51 +0000

    Refactor add_tuple function for clarity and add max_integer function with main script for testing

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    padded_a = tuple_a + (0, 0)
-    padded_b = tuple_b + (0, 0)
-    return (padded_a[0] + padded_b[0], padded_a[1] + padded_b[1])
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
