93c5cac5f35b44ae09dc20c7a6f337097dac8540
vtiquet <valentin.tiquet@gmail.com>
2025-09-08 09:42:48 +0000

    Fix max_integer function to return None for empty list instead of 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     max = 0
     if len(my_list) == 0:
-        return (0)
+        return (None)
     for i in range(len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
