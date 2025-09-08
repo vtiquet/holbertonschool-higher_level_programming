963692708c564a721209266c1ec8bee9c4e10f09
vtiquet <valentin.tiquet@gmail.com>
2025-09-08 13:04:54 +0000

    Implement best_score function to return the key with the highest value in a dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-pass
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
