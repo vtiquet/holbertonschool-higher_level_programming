4949185c8706083d3ed310c8a6ca8a1b6dd70954
vtiquet <valentin.tiquet@gmail.com>
2025-09-08 16:42:39 +0000

    Refactor roman_dictionary initialization for improved readability

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                        'C': 100, 'D': 500, 'M': 1000}
     new_int = 0
     length = len(roman_string)
 
