d5d08d8c07491418ddfab9c0d8b7a954870701c9
vtiquet <valentin.tiquet@gmail.com>
2025-09-08 10:01:38 +0000

    Fix divisible_by_2 function to use the correct list element for divisibility check

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,5 +4,5 @@ def divisible_by_2(my_list=[]):
         return (None)
     new_list = []
     for i in range(len(my_list)):
-        new_list.append(i % 2 == 0)
+        new_list.append(my_list[i] % 2 == 0)
     return (new_list)
