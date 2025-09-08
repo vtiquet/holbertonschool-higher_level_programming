88b06e256a3da1c1b233fe50dbd3a7ca5c576bda
vtiquet <valentin.tiquet@gmail.com>
2025-09-08 13:00:00 +0000

    Implement multiply_by_2 function to double the values in a dictionary

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-pass
+    new_dictionary = {}
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
+    return (new_dictionary)
