8d36921aa93c94f29e5cfcab453d1144cff84ffb
vtiquet <valentin.tiquet@gmail.com>
2025-09-09 08:54:54 +0000

    Refactor safe_print_division function to improve error handling and readability

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-""" Integers division with debug """
 def safe_print_division(a, b):
+    try:
+        safe_div = a / b
+    except (TypeError, ZeroDivisionError):
+        safe_div = None
+    finally:
+        print("Inside result: {}".format(safe_div))
+    return (safe_div)
