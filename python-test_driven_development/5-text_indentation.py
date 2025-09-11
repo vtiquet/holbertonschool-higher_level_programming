7e8cc047cfbe2b49f621b159993212005114bdef
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 12:57:09 +0000

    Refactor text_indentation function to replace while loop with for loop for improved readability

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -19,17 +19,12 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    new_text = ""
-    i = 0
-    while i < len(text):
-        new_text += text[i]
-        if text[i] in ['.', '?', ':']:
-            new_text += "\n\n"
-            i += 1
-            while i < len(text) and text[i] == ' ':
-                i += 1
-            continue
-        i += 1
-
-    print(new_text, end="")
+    x = ""
+    for i in text:
+        x += i
+        if i in ".?:":
+            print(x.strip())
+            print()
+            x = ""
+    if x.strip():
+        print(x.strip(), end="")
