172d0ef8c469aacec1a0f3a4e0fd4544577132db
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 12:10:50 +0000

    Add print_square function and corresponding test cases

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the function print_square
+This module contains the function print_square, which prints a square.
 """
 
 
@@ -18,3 +18,12 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
+    if not isinstance(size, int):
+        if isinstance(size, float):
+            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
