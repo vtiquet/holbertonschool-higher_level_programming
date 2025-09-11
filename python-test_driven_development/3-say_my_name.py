a90b479fa4b49a498ca3ccc31d78212daaab5549
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 11:59:11 +0000

    Refactor say_my_name function documentation and add type checks

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This module contains the function say_my_name
+This module contains the function say_my_name, which prints a name.
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints a name.
+    Prints a name in the format "My name is <first name> <last name>".
 
     Args:
         first_name (str): The first name.
@@ -18,3 +18,9 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If first_name or last_name is not a string.
     """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
+    
