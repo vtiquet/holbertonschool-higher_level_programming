3e738b83f844e7ff9bbfc15f650305e5d78244f6
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 12:01:22 +0000

    Fix formatting in say_my_name function output and update test cases for consistency

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -23,4 +23,3 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
-    
