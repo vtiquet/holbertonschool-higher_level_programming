63950f234b99c1148aa1e49cadba319e59b0be77
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 12:40:18 +0000

    Add text_indentation function and corresponding test cases

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This module contains the function text_indentation
+This module contains the function text_indentation.
 """
 
 
 def text_indentation(text):
     """
-    Prints text with 2 new lines after each '.', '?', and ':'.
+    Prints a text with 2 newlines after each '.', '?', and ':'.
 
     Args:
-        text (str): The text to format.
+        text (str): The text to be processed.
 
     Returns:
         None.
@@ -17,3 +17,17 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    for char in text:
+        new_text += char
+        if char in ['.', '?', ':']:
+            new_text += "\n\n"
+
+    lines = new_text.split('\n')
+    for line in lines:
+        print(line.strip(' '), end="")
+        if line != lines[-1]:
+            print("")
