2beadf60b3d099051635effc00e255e6cb22c342
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 12:35:29 +0000

    Remove handling for zero size in print_square function and simplify printing logic

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -24,10 +24,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print("")
-        return
 
     for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+        print("#" * size)
