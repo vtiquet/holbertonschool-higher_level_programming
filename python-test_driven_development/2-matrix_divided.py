f032755bab7eec2174953be87c5f3b4c38784857
vtiquet <valentin.tiquet@gmail.com>
2025-09-11 11:21:56 +0000

    Refactor matrix_divided function to improve type checks and error handling

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,3 +20,29 @@ def matrix_divided(matrix, div):
                    or if rows have different sizes, or if div is not a number.
         ZeroDivisionError: If div is 0.
     """
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
+
+    if not all(isinstance(elem, (int, float))
+               for row in matrix for elem in row):
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
+
+    if len(matrix) > 0 and not all(len(row) == len(matrix[0])
+                                   for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(elem / div, 2) for elem in row]
+        new_matrix.append(new_row)
+
+    return new_matrix
