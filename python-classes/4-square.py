1c17455f851dfb8c1f448721caadb2b80a638ae7
vtiquet <valentin.tiquet@gmail.com>
2025-09-15 08:20:41 +0000

    Implement size property with validation and area method in Square class

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,3 +4,20 @@
 
 class Square:
     """A class that defines a square with properties for size."""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
