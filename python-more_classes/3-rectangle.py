42a50579551b0fe35e9c3315937884089b4a3ec1
vtiquet <valentin.tiquet@gmail.com>
2025-09-15 11:53:28 +0000

    Fix formatting by removing unnecessary blank line in perimeter method of Rectangle class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * (self.__height + self.__width))
-    
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
