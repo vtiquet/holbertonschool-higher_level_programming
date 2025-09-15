5f0cc5b944b6379f023a666d5ff84147806d22bc
vtiquet <valentin.tiquet@gmail.com>
2025-09-15 08:11:17 +0000

    Add __init__ method to Square class with size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,3 +4,9 @@
 
 class Square:
     """A class that defines a square with size validation."""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
