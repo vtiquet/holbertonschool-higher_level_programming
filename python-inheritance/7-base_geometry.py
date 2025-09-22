9c754ce4fe288dc4202fc151e94ff5ca1c092f5f
vtiquet <valentin.tiquet@gmail.com>
2025-09-22 11:18:26 +0000

    Refactor docstrings and error messages in BaseGeometry class for clarity

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-This module defines the BaseGeometry class
-with an area method and integer_validator.
+Defines a base geometry class.
 """
 
 
 class BaseGeometry:
     """
-    A class BaseGeometry with an unimplemented
-    area method and an integer_validator.
+    BaseGeometry class with area and integer_validator methods.
     """
+
     def area(self):
+        """
+        Raises an Exception indicating the area is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
