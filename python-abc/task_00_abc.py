820a7b6ce05839c633493ec401dc34574cdf0ec9
vtiquet <valentin.tiquet@gmail.com>
2025-09-22 13:15:08 +0000

    Add initial implementations of Animal, Dog, and Cat classes

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module defines abstract and concrete classes for animals.
+"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return ("Bark")
+
+
+class Cat(Animal):
+    def sound(self):
+        return ("Meow")
