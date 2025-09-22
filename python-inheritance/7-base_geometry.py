dfde6a4e8cc816c77c464f7b236087d4e72504b7
vtiquet <valentin.tiquet@gmail.com>
2025-09-22 11:12:55 +0000

    Refactor integer_validator method in BaseGeometry to simplify type checking

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,9 +14,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is bool:
-            raise TypeError(f"{name} must be an integer")
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(name + " must be greater than 0")
