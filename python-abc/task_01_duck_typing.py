4283fa15e9cbadd0320cfb32068929ff464b1790
vtiquet <valentin.tiquet@gmail.com>
2025-09-23 07:56:42 +0000

    Ensure area and perimeter methods return absolute values in Circle and Rectangle classes

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -22,10 +22,10 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return abs(math.pi * self.radius ** 2)
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return abs(2 * math.pi * self.radius)
 
 
 class Rectangle(Shape):
@@ -34,10 +34,10 @@ class Rectangle(Shape):
         self.height = height
 
     def area(self):
-        return self.width * self.height
+        return abs(self.width * self.height)
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return abs(2 * (self.width + self.height))
 
 
 def shape_info(object):
