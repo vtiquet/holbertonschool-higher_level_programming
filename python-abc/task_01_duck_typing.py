5d46199989f4648a51ff4db495cddc0cdef8f59b
vtiquet <valentin.tiquet@gmail.com>
2025-09-23 07:51:15 +0000

    Add Shape, Circle, and Rectangle classes with area and perimeter methods

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+This module defines classes for shapes and a function to calculate their
+area and perimeter using duck typing.
+"""
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(Shape):
+    print(f"Area: {Shape.area()}")
+    print(f"Perimeter: {Shape.perimeter()}")
