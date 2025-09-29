ea930145c6e2a2460a84da03483e47e4a87d2472
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 08:31:32 +0000

    Fix file opening mode in read_file function for clarity

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """Read a text file (UTF8) and print its content to stdout."""
-    with open(filename, encoding='utf-8') as f:
-        read_data = f.read()
-        print(read_data, end="")
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
