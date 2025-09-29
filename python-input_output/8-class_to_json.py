2a7e2e42a01d78a0d5e2c158bbb369845fd8b73b
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 11:07:51 +0000

    Fix docstring formatting in class_to_json function and implement functionality to return the object's dictionary description for JSON serialization.

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Function that returns the dictionary description for JSON serialization of an object."""
+"""Function that returns the dictionary description
+for JSON serialization of an object."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description with simple data structure for JSON serialization of an object."""
-    pass
-
+    """Return the dictionary description with simple
+    data structure for JSON serialization of an object."""
+    return obj.__dict__
