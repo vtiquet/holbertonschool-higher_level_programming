cf25d28054063880f580bdbbfb43ddbe3b071989
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 08:39:46 +0000

    Refactor write_file function docstring and return statement for clarity

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a text file (UTF8) and return the number of characters written."""
+    """Write a string to a text file (UTF8)
+    and return the number of characters written."""
     with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+        f.write(text)
+        return len(text)
