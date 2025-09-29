1dc8898e24ab872bc6d3fc8e058e3c9e0fb11741
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 08:38:24 +0000

    Refactor read_file and write_file functions for consistency in encoding usage

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Read a text file (UTF8) and print its content to stdout."""
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """Write a string to a text file (UTF8) and return the number of characters written."""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
-        return len(text)
+        return f.write(text)
