5eacad61d10182df3c0c6299a8aec50bbf1db779
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 08:50:17 +0000

    Refactor from_json_string function docstring for clarity and implement functionality to return an object from a JSON string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,6 +4,6 @@ import json
 
 
 def from_json_string(my_str):
-    """Return an object (Python data structure) represented by a JSON string."""
-    pass
-
+    """Return an object (Python data structure)
+    represented by a JSON string."""
+    return json.loads(my_str)
