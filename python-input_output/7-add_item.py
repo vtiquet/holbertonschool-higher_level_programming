61c5f1eb66b09d90da8a616057cf9a78affbf00b
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 11:04:04 +0000

    Refactor add_item.py script to improve docstring formatting and implement functionality to handle command line arguments, saving them to a JSON file. Add initial JSON files for consistency.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list, and then saves them to a file."""
+"""Script that adds all arguments to a Python list,
+and then saves them to a file."""
+import sys
+import json
 
-# NOTE: This script needs the logic to import the required functions from 5- and 6-
-# and handle sys.argv, as demonstrated in the main example file.
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    # Your script logic goes here
-    pass
+filename = "add_item.json"
 
+try:
+    my_list = load_from_json_file(filename)
+
+except (FileNotFoundError, json.JSONDecodeError):
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
