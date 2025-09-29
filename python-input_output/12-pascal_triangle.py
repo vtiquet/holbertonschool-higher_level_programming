ba2c81266dcf268b5bfa15f87cbec71dbcf292cf
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 12:16:03 +0000

    Fix docstring formatting in pascal_triangle function and implement functionality to return Pascal's triangle as a list of lists

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""Function that returns a list of lists of integers representing the Pascal’s triangle of n."""
+"""Function that returns a list of lists of integers
+representing the Pascal’s triangle of n."""
 
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns a list of lists of integers
+    representing the Pascal’s triangle of n.
     Returns an empty list if n <= 0.
     """
-    pass
+    if n <= 0:
+        return []
 
+    else:
+        triangle = []
+        for i in range(n):
+            row = [1]
+            if i > 0:
+                prev_row = triangle[i - 1]
+                for j in range(len(prev_row) - 1):
+                    row.append(prev_row[j] + prev_row[j + 1])
+                row.append(1)
+            triangle.append(row)
+        return triangle
