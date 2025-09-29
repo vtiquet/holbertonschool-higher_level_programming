8099554cd3221a8f4058c884161cdc17b4f22794
vtiquet <valentin.tiquet@gmail.com>
2025-09-29 11:35:44 +0000

    Implement to_json method in Student class to retrieve dictionary representation with optional attribute filtering

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,7 +13,14 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieve a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes contained in this list must be retrieved.
+        If attrs is a list of strings,
+        only attributes contained in this list must be retrieved.
         """
-        pass
-
+        if isinstance(attrs, list):
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
