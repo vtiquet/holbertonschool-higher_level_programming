0d98bb6bbd2c8f49e989e416a3b7248b77fa6612
vtiquet <valentin.tiquet@gmail.com>
2025-10-21 07:55:15 +0000

    Add SQL script to create and populate the second_table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Creates the table second_table if it does not already exist.
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Inserts the four required records into the newly created table.
+INSERT INTO second_table (id, name, score) VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
