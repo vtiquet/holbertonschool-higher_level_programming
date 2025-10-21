0466732a292ec42f194a5bf8ed2463d8bc029d0d
vtiquet <valentin.tiquet@gmail.com>
2025-10-21 12:10:59 +0000

    Add SQL script to list all shows with linked genres using INNER JOIN

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,13 @@
+-- Lists all shows that have at least one genre linked.
+SELECT
+    t.title,
+    g.genre_id
+FROM
+    tv_shows AS t
+INNER JOIN
+    tv_show_genres AS g
+ON
+    t.id = g.show_id
+ORDER BY
+    t.title ASC,
+    g.genre_id ASC;
