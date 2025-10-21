883a7fd41f660184969b28798a97b23bcf772707
vtiquet <valentin.tiquet@gmail.com>
2025-10-21 11:29:50 +0000

    Add SQL script to list all cities in California using a subquery

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,17 @@
+-- Lists all cities of California using a subquery.
+SELECT
+    id,
+    name
+FROM
+    cities
+WHERE
+    state_id = (
+        SELECT
+            id
+        FROM
+            states
+        WHERE
+            name = 'California'
+    )
+ORDER BY
+    id ASC;
