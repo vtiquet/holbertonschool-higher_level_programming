5ad39c0c05a4c5d2a52630367b28ac5e666a31e6
vtiquet <valentin.tiquet@gmail.com>
2025-10-24 11:23:01 +0000

    Refactor 2-my_filter_states.py to improve error handling and code structure

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Displays all values in the states table where name matches the argument.
-WARNING: This script is vulnerable to SQL injection as requested by the task.
 """
 import sys
 import MySQLdb
@@ -10,28 +9,33 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         pass
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    try:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        state_name = sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}'" \
-            " ORDER BY id ASC".format(state_name)
+        query = "SELECT * FROM states WHERE name = '{}' " \
+                "ORDER BY id ASC".format(state_name)
 
-    cursor.execute(query)
+        cursor.execute(query)
 
-    for row in cursor.fetchall():
-        print(row)
+        for row in cursor.fetchall():
+            print(row)
 
-    cursor.close()
-    db.close()
+        cursor.close()
+        db.close()
+
+    except Exception as e:
+        pass
