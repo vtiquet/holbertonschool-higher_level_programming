04921f5aac7c9d141eea8cb5a1614d3bb0a60b97
vtiquet <valentin.tiquet@gmail.com>
2025-10-24 09:01:50 +0000

    Add initial Python script and README file for state selection functionality

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Takes 3 arguments: mysql username, mysql password, and database name.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+            print(row)
+
+    cursor.close()
+    db.close()
