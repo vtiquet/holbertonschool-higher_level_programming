db9608a4e6301b91e3d6e42fac20b854cc31ce4f
vtiquet <valentin.tiquet@gmail.com>
2025-10-24 12:11:49 +0000

    Add 8-model_state_fetch_first.py to print the first State object from the database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Prints the first State object from the database hbtn_0e_6_usa.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        pass
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    session = Session(engine)
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    session.close()
