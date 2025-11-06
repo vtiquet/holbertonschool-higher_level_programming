67f9f301da50bd98b2ba8185cbe4fdee2af87bf7
vtiquet <valentin.tiquet@gmail.com>
2025-11-06 10:25:06 +0000

    Add 1-multi_languages.js to print messages in multiple programming languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const meineVar = 'Python is cool';
+const maVar = 'JavaScript is amazing';
+console.log(myVar);
+console.log(meineVar);
+console.log(maVar);
