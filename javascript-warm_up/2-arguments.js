975944558c629152bbc370bedd46dbb8c01161d6
vtiquet <valentin.tiquet@gmail.com>
2025-11-06 12:21:48 +0000

    Fix condition checks for argument length in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const numArgs = process.argv.slice(2);
 
-if (numArgs === 0) {
+if (numArgs.length === 0) {
   console.log('No argument');
-} else if (numArgs === 1) {
+} else if (numArgs.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
