23bd7aae44b5808005b73ef8339f4f5b007d40cb
vtiquet <valentin.tiquet@gmail.com>
2025-11-17 09:29:05 +0000

    Fix inconsistent quotation marks in LockedClass __slots__ definition and update docstring formatting

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Prevents dynamic creation of new instance attributes,
+except if the new instance attribute is called first_name.
+"""
+
+
 class LockedClass:
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Initialize class instance."""
+        pass
