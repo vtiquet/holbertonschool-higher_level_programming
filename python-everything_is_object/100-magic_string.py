45629709a914869f9523d12f1d297b990448dc27
vtiquet <valentin.tiquet@gmail.com>
2025-11-17 09:17:07 +0000

    Refactor magic_string function to use an attribute for counter management

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def magic_string(magic_string.counter=[0]):
-    magic_string.counter[0] += 1
-    return (", ".join(["BestSchool"] * magic_string.counter[0]))
+def magic_string():
+    if not hasattr(magic_string, 'counter'):
+        magic_string.counter = 0
+    magic_string.counter += 1
+    return (", ".join(["BestSchool"] * magic_string.counter))
